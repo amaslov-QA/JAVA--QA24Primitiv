b7447df59d2d44b920e43f2020be44bfe9c98b34
aa_maslov <smart.maslov@gmail.com>
2021-10-04 12:07:46 +0300

    добавлена программа для расчета бонусов

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
+import java.net.SocketOption;
+
 public class Main {
+    public static void main(String[] args) {
+
+        int amountTickets = 635_593;
+        int bonus = 20;
+        int mile = amountTickets/bonus;
+        System.out.println(mile);
+    }
 }
+
